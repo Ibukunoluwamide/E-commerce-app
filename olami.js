d4ab3adb6888ebe6d8eda9be7d871ffc6cd4bd08
Alao Olamide Ezekiel <olamidealao22@gmail.com>
2023-03-01 15:52:33 +0100

    Complete footers and added itemInfo.html

--- olami.js
+++ olami.js
@@ -16,3 +16,21 @@ const searchBarrr = ()=>{
         alert("Searching...")
     
 }
+let itemArray = []
+let addToCart = document.querySelectorAll(".item")
+// console.log(addToCart)
+addToCart.forEach((eachItem)=>{
+    eachItem.addEventListener("click",()=>{
+        // console.log(eachItem.firstElementChild.src);
+         let itemDetails = {
+            itemImage: eachItem.firstElementChild.src,
+            itemText: eachItem.innerHTML
+         }
+        itemArray.push(itemDetails)
+        console.log(itemArray);
+        localStorage.setItem("itemm",JSON.stringify(itemArray))
+        window.location.href = "itemInfo.html"
+        // alert("carrt")
+
+})
+})
